5d08cb335bfafcacf4ee19db9da9802b76bd1915
Charles The Great <27988707+CharlesTheGreat77@users.noreply.github.com>
2023-11-15 10:21:22 -0800

    Update carrier_lookup.py

--- core/carrier_lookup.py
+++ core/carrier_lookup.py
@@ -1,54 +1,54 @@
 from bs4 import BeautifulSoup
-from playwright_stealth import stealth_sync
 from playwright.sync_api import sync_playwright
-import re
+import pytesseract, requests, random
+from io import BytesIO
+from PIL import Image, ImageFilter, ImageEnhance
 
-
-def get_carrier_from_spydialer(phone_number):
+def get_carrier_from_carrierlookup(phone_number):
     with sync_playwright() as p:
-        browser = p.webkit.launch()
-        context = browser.new_context()
+        devices = p.devices
+        rand_device = random.choice(list(devices.keys()))
+        print(f'[*] Using device: {rand_device}')
+        device = p.devices[rand_device]
+        browser = p.webkit.launch(headless=False)
+        context = browser.new_context(**device,)
         page = context.new_page()
-        stealth_sync(page)
-        page.goto('https://spydialer.com')
-        page.fill('input#SearchTextBox', phone_number)
-        page.click('input[value="Search"]')
-        print("[*] Waiting for 6 seconds for spydialer search button to load")
-        page.wait_for_timeout(6000)
-        page.click('input[value="Search"]')
-        print('[*] Waiting 10 seconds for other spydialer search button to load\n')
-        page.wait_for_timeout(10000)
+        page.goto('https://currentcarrierlookup.com')
+        page.wait_for_timeout(random.randint(1000,2000))
         html_content = page.content()
+        soup = BeautifulSoup(html_content, 'html.parser')
+        captcha = soup.select_one('form img.cimg')['src']
+        get_captcha = requests.get(captcha)
+        img = Image.open(BytesIO(get_captcha.content))
+        img.convert('L')
+        img = img.filter(ImageFilter.MedianFilter())
+        enchancer = ImageEnhance.Contrast(img)
+        img = enchancer.enhance(2)
+        img.convert('1')
+        img.save('temp.png')
+        captcha_text = pytesseract.image_to_string(img, config='--oem 3 --psm 11 outputbase aplphanumeric')
+        page.fill('#tn-tel', phone_number)
+        page.wait_for_timeout(random.randint(1500,3000))
+        page.fill("#bcap", captcha_text)
+        page.wait_for_timeout(random.randint(1500,2500))
+        page.click('input[value="check"]')
+        page.wait_for_selector(".date-time span")
+        carrier = page.text_content(".date-time span")
+        sms_gateway = get_gateway_from_carrier(carrier)
+        page.wait_for_timeout(random.randint(1500,2500))
         browser.close()
-        sms_gateway = get_carrier_from_html(html_content)
 
         return sms_gateway
     
 
-def get_carrier_from_html(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    #name_element = soup.find('a', id='ctl00_ContentPlaceHolder1_NameLinkButton')
-    #name = name_element.text if name_element else None
-    message_element = soup.find('span', id='ctl00_ContentPlaceHolder1_DataMessageLabel')
-    message = message_element.text if message_element else None
-    carrier = re.findall(r'\b[A-Z]+(?:\W+[A-Z]+)*', message)
-    provider = carrier[1].split()
-
-    if 'AT&T' in provider[0]:
-        carrier = '@mms.att.net'
-    elif 'METROPCS' in provider[0]:
-        carrier = '@mymetropcs.com' 
-    elif 'TMOBILE' in provider[0]:
-        carrier = '@tmomail.net'
-    elif 'SPRINTPCS' in provider[0]:
-        carrier = '@pm.sprint.com'
-    elif 'VERIZON' in provider[0]:
-        carrier = '@vzwpix.com'
-    elif 'CRICKET' in provider[0]:
-        carrier = '@mms.cricketwireless.net'
-    elif 'US' in provider:
-        carrier = '@mms.uscc.net'
-    else:
-        carrier = '@tmomail.net'
-
-    return carrier
+def get_gateway_from_carrier(carrier):
+    if 'Cingular Wireless/2' in carrier:
+        sms_gateway = '@txt.att.net'
+    elif 'Verizon Wireless:6006 - SVR/2' in carrier:
+        sms_gateway = '@vtext.com'
+    elif 'T-Mobile US-SVR-10X/2' in carrier:
+        sms_gateway = '@tmomail.net'
+    elif 'Metro PCS Communications Inc-SVR-10X/2' in carrier:
+        sms_gateway = '@mymetropcs.com'
+    
+    return sms_gateway
